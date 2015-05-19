c55dc1d4673c6c35d60ad3fdaecc1f46482e4556
thanhvc <thanhvc@exoplatform.com>
2015-05-19 14:52:31 +0700

    Adds StreamItem mapping to Activity entity

--- lib/src/main/java/org/exoplatform/social/addons/storage/entity/Activity.java
+++ lib/src/main/java/org/exoplatform/social/addons/storage/entity/Activity.java
@@ -68,6 +68,10 @@ public class Activity extends BaseActivity {
   @OneToMany(cascade=CascadeType.ALL, mappedBy="activity", fetch=FetchType.LAZY)
   @OrderBy("posted DESC")
   private List<Comment> comments;
+  
+  @OneToMany(cascade=CascadeType.ALL, mappedBy="stream", fetch=FetchType.LAZY)
+  @OrderBy("id DESC")
+  private List<StreamItem> streamItems;
 
   public Activity() {
     setPosted(new Date());
@@ -114,12 +118,36 @@ public class Activity extends BaseActivity {
     this.comments = comments;
   }
 
+  /**
+   * Adds the comment item entity to this activity
+   * @param item the stream item
+   */
   public void addComment(Comment comment) {
     if (this.comments == null) {
       this.comments = new ArrayList<Comment>();
     }
     comment.setActivity(this);
     this.comments.add(comment);
+  }
+  
+  public List<StreamItem> getStreamItems() {
+    return streamItems;
+  }
+
+  public void setStreamItems(List<StreamItem> streamItems) {
+    this.streamItems = streamItems;
+  }
+
+  /**
+   * Adds the stream item entity to this activity
+   * @param item the stream item
+   */
+  public void addStreamItem(StreamItem item) {
+    if (this.streamItems == null) {
+      this.streamItems = new ArrayList<StreamItem>();
+    }
+    item.setActivity(this);
+    this.streamItems.add(item);
   }
 
   public Long getId() {
